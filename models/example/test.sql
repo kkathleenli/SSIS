8ee4e5a27d9983af29078a2c4a633eaba8cd63a0
Kathleen Li <chli@kissusa.com>
2024-04-24 05:07:41 +0000

    test loop cast change data type

--- models/example/test.sql
+++ models/example/test.sql
@@ -1,1 +1,11 @@
-{{condition_check('"ZMATNR"')}}
+select
+    NAME,
+    CAST(RIGHT(ADDRESS,6) AS VARCHAR(6)) AS PIN
+FROM {{ source('PERSON','PERSON_DETAILS')}}
+
+
+{{% set to_date=['"0CALDAY"','"0DOC_DATE"','"0CREATEDON"','"0PSTNG_DATE"']%}}
+
+{% for i in to_date %}
+    {% if {{condition_check(i)}}='change' %}
+        

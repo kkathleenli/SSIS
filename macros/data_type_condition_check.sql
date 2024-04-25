e71b1721138ecfbfc021caf4f8b9168f02c1b163
Kathleen Li <chli@kissusa.com>
2024-04-25 03:24:02 +0000

    [[for loop test]]

--- macros/data_type_condition_check.sql
+++ macros/data_type_condition_check.sql
@@ -1,8 +1,8 @@
-{% macro condition_check(col_name) %}
+{% macro data_type_condition_check(col_name) %}
 
 with temp as (
 SELECT DISTINCT {{col_name}} AS CHECK_VALUE 
-FROM {{ source('snowflake_TEST', 'TEST_BILLING_CL_20240403_BACKUP') }} 
+FROM {{ source('snowflake_TEST', 'TEST_BILLING_CL_20240403_BACKUP') }}
 WHERE {{col_name}} LIKE '%#%'
 )
 select case when COUNT(CHECK_VALUE) > 0 then 'not change' else 'change' end 
---
+++ models/example/change_data_type.sql
@@ -0,0 +1,15 @@
+{%- set to_date=['"0CALDAY"','"0DOC_DATE"','"0CREATEDON"','"0PSTNG_DATE"'] -%}
+
+{%- for col_name in to_date -%}
+    {%- set condition_result = {{data_type_condition_check({{ col_name }})}} -%}
+    {%- set status = condition_result.rows[0][0] if condition_result.rows|length > 0 else 'not change' -%}
+
+    {%- if status == 'change' -%}
+        {%- set change_to_date -%}
+            SELECT CAST({{ col_name }} AS DATE) AS {{ col_name }}
+            FROM {{ ref('create_table_test') }}
+        {%- endset -%}
+        {%- do run_query(change_to_date) -%}
+    {%- endif -%}
+{%- endfor -%}
+

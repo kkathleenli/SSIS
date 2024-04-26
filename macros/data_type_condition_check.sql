3df1dc180397e483ccf75db244836e0f94046d13
Kathleen Li <chli@kissusa.com>
2024-04-26 21:19:26 +0000

    [test]

--- macros/data_type_condition_check.sql
+++ macros/data_type_condition_check.sql
@@ -1,11 +1,14 @@
 {% macro data_type_condition_check(col_name) %}
 
-with temp as (
-SELECT DISTINCT {{col_name}} AS CHECK_VALUE 
-FROM {{ source('snowflake_TEST', 'TEST_BILLING_CL_20240403_BACKUP') }}
-WHERE {{col_name}} LIKE '%#%'
-)
-select case when COUNT(CHECK_VALUE) > 0 then 'not change' else 'change' end 
-from temp
+   {% call statement('query',fetch_result=True) %}
+   select case when COUNT(CHECK_VALUE) > 0 then 'not change' else 'change' end
+   from (
+   SELECT DISTINCT {{col_name}} AS CHECK_VALUE 
+   FROM {{ source('snowflake_TEST', 'TEST_BILLING_CL_20240403_BACKUP') }}
+   WHERE {{col_name}} LIKE '%#%'
+   )
+   {% endcall %}
+
+   {% set results = load_result('query')['data'] %}
 
 {% endmacro %}
--- models/example/change_data_type.sql
+++ models/example/change_data_type.sql
@@ -1,17 +1,24 @@
 {%- set to_date=['"0CALDAY"','"0DOC_DATE"','"0CREATEDON"','"0PSTNG_DATE"'] -%}
+{%- set cols = adapter.get_columns_in_relation({{ source('snowflake_TEST', 'TEST_BILLING_CL_20240403_BACKUP') }}) %}
 
-{%- for col_name in to_date -%}
-    {%- set condition_result = {{ data_type_condition_check( {{ col_name }} ) }} -%}
-    {%- set status = condition_result.rows[0][0] if condition_result.rows|length > 0 else 'not change' -%}
+{%- for col_name in cols -%}
+    {% if col_name in to_date %}
+    {% call statement('query',fetch_result=True) %}
+    select case when COUNT(CHECK_VALUE) > 0 then 'not change' else 'change' end
+    from (
+    SELECT DISTINCT {{col_name}} AS CHECK_VALUE 
+    FROM {{ source('snowflake_TEST', 'TEST_BILLING_CL_20240403_BACKUP') }}
+    WHERE {{col_name}} LIKE '%#%'
+    )
+    {% endcall %}
+    (% endif %)
 
-    {%- if status == 'change' -%}
-        {%- set change_to_date -%}
-            SELECT CAST({{ col_name }} AS DATE) AS {{ col_name }}
-            FROM {{ ref('create_table_test') }}
-        {%- endset -%}
-        {%- do run_query(change_to_date) -%}
-    {%- endif -%}
+    {% set results = load_result('query')['data'][0][0] %}
+
+    {% if results == 'change' and not loop.last %}
+    {% call statement(change_to_date) -%}
+        SELECT CAST({{col_name}} AS DATE) AS {{col_name}}
+        FROM {{ source('snowflake_TEST', 'TEST_BILLING_CL_20240403_BACKUP') }}
+    {% endcall %}
+    {% endif %}
 {%- endfor -%}
-
-select *
-from {{data_type_condition_check({{col_name}})}}
